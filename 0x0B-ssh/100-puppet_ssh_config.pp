3ddd0c9e0d6660e570b4a17919b54e28f0a0539b
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-08-11 19:02:00 -0700

    fix: made the exec properly without silly work arounds

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,8 @@
 # change ssh_config file to allow for ssh Only connection to private server.
 
-exec { 'echo "Host 52.204.151.72
+exec {'echo':
+  command  => 'echo "Host 52.204.151.72
     IdentityFile ~/.ssh/school
-    PasswordAuthentication no" >> /etc/ssh/ssh_config':
-  path => "/bin"
+    PasswordAuthentication no" >> /etc/ssh/ssh_config',
+  provider => 'shell',
 }
