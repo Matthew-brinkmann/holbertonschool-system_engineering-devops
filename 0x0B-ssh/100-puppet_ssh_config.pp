c3a77fd4b63df4c6015860dd0c462ec1537cc6a4
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-08-11 18:44:31 -0700

    fix: add comment

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,3 +1,5 @@
+# change ssh_config file to allow for ssh Only connection to private server.
+
 exec { 'echo "Host 52.204.151.72
     IdentityFile ~/.ssh/school
     PasswordAuthentication no" >> /etc/ssh/ssh_config':
