374abf4d1a53fa56c486ea4849c9a90dbc54277f
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-08-15 19:39:21 -0700

    fix: Build order and captilisation errors

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -5,24 +5,35 @@ package { 'nginx':
   provider => 'apt',
 }
 
+exec { 'nginx start':
+  command  => 'service nginx start',
+  provider => 'shell',
+  require =>  Package['nginx'],
+}
+
 exec { 'rm':
   command  => 'rm /var/www/html/index.nginx-debian.html',
   onlyif   => 'test -e /var/www/html/index.nginx-debian.html',
   provider => 'shell',
+  require =>  Exec['nginx start'],
 }
 
 file { 'school':
   path    => '/var/www/html/index.html',
   mode    => '0644',
   content => 'Hellow World',
+  require =>  Exec['rm'],
 }
 
 exec { 'sed':
   command  => 'sed -i "/server_name _;/a\\\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+  onlyif => 'test -e /etc/nginx/sites-available/default',
   provider => 'shell',
+  require =>  File['school'],
 }
 
-exec { 'nginx start':
-  command  => 'service nginx start',
+exec { 'nginx restart':
+  command  => 'service nginx restart',
   provider => 'shell',
+  require =>  Exec['sed'],
 }
