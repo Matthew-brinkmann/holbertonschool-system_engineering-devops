95204597f9fbab15d34f626b06d07912f78d5fe3
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-09-26 07:45:53 +1000

    build: task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""second task for advanced API"""
+import requests
+
+
+def top_ten(subreddit):
+    """Returns the title of the first
+    10 hot posts to a subreddit or
+    None if not a valid subreddit"""
+
+    if subreddit is None or not type(str):
+        print (None)
+        return
+
+    apiUrlToCall = "http://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headersToCall = {"User-Agent":
+                     "Python3:School_Project:v1.0 (by /u/theHilariousMrcbtdx)"}
+    requestOptions = {"limit": 10}
+    apiRequest = requests.get(apiUrlToCall,
+                              headers=headersToCall,
+                              verify=False,
+                              params=requestOptions).json()
+    allPosts = apiRequest.get("data", {}).get("children", 0)
+    if allPosts is None:
+        print(None)
+    else:
+        for singlePost in allPosts:
+            print(singlePost.get('data', {}).
+                             get('title'))
