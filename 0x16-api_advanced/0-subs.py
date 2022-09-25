00bfba02105a385a32f775673b755b7e08b393a2
Matthew Brinkmann <matthew.brinkmann@hotmail.com>
2022-09-26 07:27:11 +1000

    initial push && Build: task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""first task for advanced API"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Returns the number of subscribers
+    to a subreddit or 0(zero) if not a
+    valid subreddit"""
+
+    if subreddit is None or not type(str):
+        return (0)
+
+    apiUrlToCall = "http://www.reddit.com/r/{}/about.json".format(subreddit)
+    headersToCall = {"User-Agent":
+                     "Python3:School_Project:v1.0 (by /u/theHilariousMrcbtdx)"}
+    apiRequest = requests.get(apiUrlToCall,
+                              headers=headersToCall).json()
+    totalSubs = apiRequest.get("data", {}).get("subscribers", 0)
+
+    return (totalSubs)
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,3 @@
+# Readme for Porject 0x16. API advanced
+## By Matthew Brinkmann
+Will add more if needed
